17b63c4b50e20de242ef40a34eefb0a4ea5f21e0
natnael-OQP <makiya112545@gmail.com>
2022-06-19 00:31:26 +0300

    LandingPage created

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../components/Banner'
+import Header from '../components/Header'
+
+function LandingPage() {
+    return (
+        <div className="min-h-screen">
+            <Header />
+            <Banner />
+        </div>
+    )
+}
+
+export default LandingPage
