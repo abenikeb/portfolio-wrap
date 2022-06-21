dab341832c6acef807fae8c07d9689781ad52994
natnael-OQP <makiya112545@gmail.com>
2022-06-21 19:15:51 +0300

    import pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import AboutPage from './pages/AboutPage'
+import GetInTouch from './pages/GetInTouch'
 import LandingPage from './pages/LandingPage'
+import Skill from './pages/Skill'
+import Testimonials from './pages/Testimonials'
+import Works from './pages/Works'
 
 function App() {
     return (
         <>
             <LandingPage />
             <AboutPage />
+            <Skill />
+            <Works />
+            <GetInTouch />
         </>
     )
 }
